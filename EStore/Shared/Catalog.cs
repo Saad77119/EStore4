99a32bf3f5b6ad9bc8fbf43549da920a0ae5df75
Muhammad Haseeb <muhammadhaseebtcf@gmail.com>
2021-06-04 16:08:40 +0500

    first commit

---
+++ EStore/Shared/Catalog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore.Shared
+{
+    public class Catalog
+    {
+        public List<Product> Products { get; set; }
+
+        public Product GetProduct(int id) => Products.SingleOrDefault(p => p.Id == id);
+
+    }
+}
---
+++ EStore/Shared/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore.Shared
+{
+    public enum MainCategory
+    {
+        MensFashion,
+        WomensFashion,
+        Electronics
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public MainCategory  MainCategory { get; set; }
+
+        public string subCategory { get; set; }
+
+        public int ShopID { get; set; }
+        
+        public Product(int id, string name, string description,decimal Price, MainCategory mainCategory,string subcategory,int ShopID)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Price = Price;
+            this.MainCategory = mainCategory;
+            this.subCategory = subCategory;
+            this.ShopID = ShopID;
+        }
+    }
+}
